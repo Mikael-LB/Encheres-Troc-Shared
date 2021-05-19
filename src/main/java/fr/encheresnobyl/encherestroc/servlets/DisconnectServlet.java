c4eb7b6e4b4d32ff0d155ad105a8d4a3bd9cc98d
Mikael-LB <mikgitlab@gmail.com>
2021-05-19 16:48:34 +0200

    add session timeout of 5 minutes

---
+++ src/main/java/fr/encheresnobyl/encherestroc/servlets/DisconnectServlet.java
@@ -0,0 +1,35 @@
+package fr.encheresnobyl.encherestroc.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DisconnectServlet
+ * @author mlebris2021
+ * Servlet which the get and post method for the disconnect feature
+ */
+@WebServlet({ "/DisconnectServlet", "/disconnect" })
+public class DisconnectServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
