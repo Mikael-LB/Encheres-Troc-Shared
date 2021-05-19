61fa37cd15727217ad25de18485ff0d15a7a5431
Mikael-LB <mikgitlab@gmail.com>
2021-05-19 14:36:17 +0200

    fix wrong number

--- src/main/java/fr/encheresnobyl/encherestroc/bll/CodesErreurBLL.java
+++ src/main/java/fr/encheresnobyl/encherestroc/bll/CodesErreurBLL.java
@@ -27,9 +27,9 @@ public abstract class CodesErreurBLL {
 	public static final int EMPTY_PARAM = 20_011;
 	public static final int PSEUDO_CHAR_NOT_ALLOWED = 20_012;
 	public static final int PSEUDO_EXIST = 20_013;
-	public static final int EMAIL_INVALID = 20_0014;
-	public static final int EMAIL_ALREADY_EXIST = 20_0015;
-	public static final int PASSWORDS_DONT_MATCH = 20_0016;
+	public static final int EMAIL_INVALID = 20_014;
+	public static final int EMAIL_ALREADY_EXIST = 20_015;
+	public static final int PASSWORDS_DONT_MATCH = 20_016;
 
 
 
